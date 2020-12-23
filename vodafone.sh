8c5ea5465fd5875e7c32b738c3593928eebf0c96
Dimitri <schiavone.dimitri.2@gmail.com>
2020-12-23 19:42:12 +0100

    Added possibility to specify to not check ssl certificates, added possibility to log through openwrt logging daemon or to shell, general fixes

--- vodafone.sh
+++ vodafone.sh
@@ -1,88 +1,101 @@
 #!/bin/bash
-#your login username
+#your login username urlencoded
 USERNAME=""
-#your login password
+#your login password urlencoded
 PASSWORD=""
-#the directory where you have saved the script starting from the root and with an ending slash e.g /etc/scripts/
+#the directory where you have saved the script (can be either an absolute pathname or a relative one) e.g /my/path or my/path (no need for ending slash)
 DIR=""
-#the logname you wish to assign to the script when getting log messages
+#the logname you wish to assign to the script when getting log messages (only for openwrt)
 LOGNAME=""
+#set this variable to yes if you are running this on openwrt to log messages through the openwrt logging system otherwise the logs will be printed to standard output
+OPENWRT=""
+#set this to no if you don't want wget to check certificates (if you use this on a system that relies on connectivity to get the date and time and you run this on boot the ssl check will fail resulting in connection errors)
+SSL=""
 
-wget --server-response --append-output=$DIR'vodafone.txt' -qO/dev/null 'http://connectivitycheck.gstatic.com/'
+if [ "$DIR" = "" ];then
 
-if test -f $DIR'vodafone.txt';then
+    DIR="."
+    
+fi
 
-	REFRESH=$(cat $DIR'vodafone.txt' | grep "Location: https://it.portal.vodafone-wifi.com/jcp/it?res=notyet" | cut -d= -f2 | cut -d\& -f1)
+if [ "$OPENWRT" = "yes" ];then
+    
+    LOG="logger -p info -t $LOGNAME"
+    
+else
+
+    LOG="echo"
+    
+fi
+
+if [ "$SSL" = "no" ];then
+
+    WGETSSL="--no-check-certificate"
+
+fi
+
+wget $WGETSSL --server-response --append-output=$DIR'/vodafone.txt' -qO/dev/null 'http://connectivitycheck.gstatic.com/'
+
+
+if test -f $DIR'/vodafone.txt';then
+
+	REFRESH=$(cat $DIR'/vodafone.txt' | grep "Location: https://it.portal.vodafone-wifi.com/jcp/it?res=notyet" | cut -d= -f2 | cut -d\& -f1)
 
 else
 
-	#below line is for logging on openwrt, change it according to your system and needs.
-	logger -p info -t "$DIR$LOGNAME" "wget failed to create file $DIRvodafone.txt"
+	$LOG "$LOGNAME" "wget failed to create file $DIR/vodafone.txt"
 
 fi
 
 if [ "$REFRESH" = "notyet" ];then
 
-	UAMIP=$(cat $DIR'vodafone.txt' | grep "Location: https://it.portal.vodafone-wifi.com/jcp/it?res=notyet" | cut -d\& -f2)
-	UAMPORT=$(cat $DIR'vodafone.txt' | grep "Location: https://it.portal.vodafone-wifi.com/jcp/it?res=notyet" | cut -d\& -f3)
-	NASID=$(cat $DIR'vodafone.txt' | grep "Location: https://it.portal.vodafone-wifi.com/jcp/it?res=notyet" | cut -d\& -f4)
-	CHALLENGE=$(cat $DIR'vodafone.txt' | grep "Location: https://it.portal.vodafone-wifi.com/jcp/it?res=notyet" | cut -d\& -f5)
-	MAC=$(cat $DIR'vodafone.txt' | grep "Location: https://it.portal.vodafone-wifi.com/jcp/it?res=notyet" | cut -d\& -f6)
-	IP=$(cat $DIR'vodafone.txt' | grep "Location: https://it.portal.vodafone-wifi.com/jcp/it?res=notyet" | cut -d\& -f7)
+	UAMIP=$(cat $DIR'/vodafone.txt' | grep "Location: https://it.portal.vodafone-wifi.com/jcp/it?res=notyet" | cut -d\& -f2)
+	UAMPORT=$(cat $DIR'/vodafone.txt' | grep "Location: https://it.portal.vodafone-wifi.com/jcp/it?res=notyet" | cut -d\& -f3)
+	NASID=$(cat $DIR'/vodafone.txt' | grep "Location: https://it.portal.vodafone-wifi.com/jcp/it?res=notyet" | cut -d\& -f4)
+	CHALLENGE=$(cat $DIR'/vodafone.txt' | grep "Location: https://it.portal.vodafone-wifi.com/jcp/it?res=notyet" | cut -d\& -f5)
+	MAC=$(cat $DIR'/vodafone.txt' | grep "Location: https://it.portal.vodafone-wifi.com/jcp/it?res=notyet" | cut -d\& -f6)
+	IP=$(cat $DIR'/vodafone.txt' | grep "Location: https://it.portal.vodafone-wifi.com/jcp/it?res=notyet" | cut -d\& -f7)
 	
-	if test -f $DIR'cookies.txt';then
+	if test -f $DIR'/cookies.txt';then
 
-		 wget --save-cookies $DIR'cookies.txt' --keep-session-cookies --load-cookies=$DIR'cookies.txt' --server-response --append-output=$DIR'vodafone2.txt' -qO/dev/null --post-data 'userFake='$USERNAME'&UserName=VF_IT%2F'$USERNAME'&Password='$PASSWORD'&_rememberMe=on' 'https://it.portal.vodafone-wifi.com/jcp/it?res=login&'$NASID'&'$UAMIP'&'$UAMPORT'&'$MAC'&'$CHALLENGE'&'$IP
+		 wget $WGETSSL --save-cookies $DIR'/cookies.txt' --keep-session-cookies --load-cookies=$DIR'/cookies.txt' --server-response --append-output=$DIR'/vodafone2.txt' -qO/dev/null --post-data 'userFake='$USERNAME'&UserName=VF_IT%2F'$USERNAME'&Password='$PASSWORD'&_rememberMe=on' 'https://it.portal.vodafone-wifi.com/jcp/it?res=login&'$NASID'&'$UAMIP'&'$UAMPORT'&'$MAC'&'$CHALLENGE'&'$IP
 	
 	else
 
-		wget --save-cookies $DIR'cookies.txt' --keep-session-cookies --server-response --append-output=$DIR'vodafone2.txt' -qO/dev/null --post-data 'userFake='$USERNAME'&UserName=VF_IT%2F'$USERNAME'&Password='$PASSWORD'&_rememberMe=on' 'https://it.portal.vodafone-wifi.com/jcp/it?res=login&'$NASID'&'$UAMIP'&'$UAMPORT'&'$MAC'&'$CHALLENGE'&'$IP
+		wget $WGETSSL --save-cookies $DIR'/cookies.txt' --keep-session-cookies --server-response --append-output=$DIR'/vodafone2.txt' -qO/dev/null --post-data 'userFake='$USERNAME'&UserName=VF_IT%2F'$USERNAME'&Password='$PASSWORD'&_rememberMe=on' 'https://it.portal.vodafone-wifi.com/jcp/it?res=login&'$NASID'&'$UAMIP'&'$UAMPORT'&'$MAC'&'$CHALLENGE'&'$IP
 
-		if  ! -f $DIR'cookies.txt';then
+		if  ! -f $DIR'/cookies.txt';then
 
-			#below line is for logging on openwrt, change it according to your system and needs.
-			logger -p info -t "$DIR$LOGNAME" "wget failed to create file $DIRcookies.txt"
+			$LOG "$LOGNAME" "wget failed to create file $DIR/cookies.txt"
 		
 		fi
 
 	fi
 	
-	if test -f $DIR'vodafone2.txt';then
+	if test -f $DIR'/vodafone2.txt';then
 
-		SUCCESS=$(cat $DIR'vodafone2.txt' | grep "Location: https:" | cut -d\& -f1 | cut -d= -f2)
+		SUCCESS=$(cat $DIR'/vodafone2.txt' | grep "Location: https:" | cut -d\& -f1 | cut -d= -f2)
 
 		if [ "$SUCCESS" = "success" ];then
 
-			#below line is for logging on openwrt, change it according to your system and needs.
-			logger -p info -t "$DIR$LOGNAME" "login successfull"
+			$LOG "$LOGNAME" "login successfull"
 			
 		else
 
-			#below line is for logging on openwrt, change it according to your system and needs.
-			logger -p info -t "$DIR$LOGNAME" "login failed"
+			$LOG "$LOGNAME" "login failed"
 
 		fi
 	
 	else
 		
-		#below line is for logging on openwrt, change it according to your system and needs.
-		logger -p info -t "$DIR$LOGNAME" "wget failed to create file $DIRvodafone2.txt"
+		$LOG "$LOGNAME" "wget failed to create file $DIR/vodafone2.txt"
 
 	fi
 
 else
 	
-	 if [ "$REFRESH" = "" ];then
+    $LOG "$LOGNAME" "connection is still alive or your wireless connectivity has problems (check that you are connected to a Vodafone-Wi-Fi ap and/or your dns settings)"
         
-        #below line is for logging on openwrt, change it according to your system and needs.   
-		logger -p info -t "$DIR$LOGNAME" "general error, check your wireless connectivity and/or dns settings"
-    
-    else    
-        #below line is for logging on openwrt, change it according to your system and needs.
-	    logger -p info -t "$DIR$LOGNAME" "wget failed to create file $DIRvodafone2.txt"
-    
-    fi
-
 fi
 
-rm -rf $DIR'vodafone.txt' $DIR'vodafone2.txt'
+rm -rf $DIR'/vodafone.txt' $DIR'/vodafone2.txt'
